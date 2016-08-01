1a39a666ada45614e2d1718a92ce9897de071a1d
Maciej Folik <maciej.folik@misys.com>
2016-08-01 15:37:12 +0200

    added DaoEnergy Interface

---
+++ Kamienica/src/main/java/kamienica/feature/reading/ReadingEnergyDao.java
@@ -0,0 +1,40 @@
+package kamienica.feature.reading;
+
+import java.util.List;
+import java.util.Set;
+
+import org.joda.time.LocalDate;
+
+import kamienica.dao.DaoInterface;
+import kamienica.feature.apartment.Apartment;
+import kamienica.feature.invoice.InvoiceEnergy;
+
+public interface ReadingEnergyDao extends DaoInterface<ReadingEnergy> {
+
+	public List<ReadingEnergy> getByDate(String date);
+
+	public List<ReadingEnergy> getPrevious(String date, Set<Long> meterId);
+
+	public List<ReadingEnergy> getLatestList(Set<Long> meterId);
+
+	public List<ReadingEnergy> getListForTenant(Apartment apartment);
+
+	/**
+	 * @return List<R extends ReadingAbsract
+	 * 
+	 *         Returns list of unresolved reading for main counter. This list is
+	 *         used for new Incoives
+	 */
+	public List<ReadingEnergy> getUnresolvedReadings();
+
+	public void resolveReadings(InvoiceEnergy invoice);
+
+	public void unresolveReadings(InvoiceEnergy invoice);
+
+	public int countDaysFromLastReading();
+
+	public void deleteLatestReadings(LocalDate date);
+
+	public LocalDate getLatestDate();
+
+}
