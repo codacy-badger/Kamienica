8f5e47deaa478eb18f351217ea237323651e4e75
macfol <folik@wp.pl>
2016-12-21 18:37:14 +0100

    testing angular url

--- src/main/webapp/static/angular/apartment/apartment_service.js
+++ src/main/webapp/static/angular/apartment/apartment_service.js
@@ -4,7 +4,8 @@
 
 App.factory('Apartment', ['$resource', function ($resource) {
     return $resource(
-    		'/Kamienica/api/v1/apartments/:id.json', 
+    		//'/Kamienica/api/v1/apartments/:id.json', 
+    		'https://macfol.herokuapp.com/api/v1/apartments.json',
     		{id: '@id'},//Handy for update & delete. id will be set with id of instance
     		{
     			query:  {method:'GET', isArray:true},
