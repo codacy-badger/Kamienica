fededd2202cf00fbc77c4d62b17782333434cc6c
Maciej Folik <maciej.folik@misys.com>
2017-01-09 11:00:48 +0100

    added more info to userStatus

--- src/main/java/kamienica/model/UserDetails.java
+++ src/main/java/kamienica/model/UserDetails.java
@@ -3,10 +3,10 @@ package kamienica.model;
 
 import kamienica.core.enums.Status;
 import kamienica.core.enums.UserRole;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity(name = "USER_DETAILS")
 public class UserDetails {
@@ -14,6 +14,47 @@ public class UserDetails {
     @Id
     @GeneratedValue
     private Long id;
-    private UserRole role;
-    private Status status;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.TENANT;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.ACTIVE;
+    @Length(min = 5, message = "Hasło musi mieć minimum 5 znaków")
+    @Column(nullable = false)
+    @NotEmpty(message = "Wprowadź hasło")
+    private String password = "witaj";
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
